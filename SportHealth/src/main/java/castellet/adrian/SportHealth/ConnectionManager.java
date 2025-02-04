3e09e57b036bba3a5d5628c87918b072d783008d
adri <04adrianc@inscastellet.cat>
2025-02-04 19:21:50 +0100

    Menu Afegit

--- SportHealth/src/main/java/castellet/adrian/SportHealth/ConnectionManager.java
+++ SportHealth/src/main/java/castellet/adrian/SportHealth/ConnectionManager.java
@@ -1,6 +1,8 @@
 package castellet.adrian.SportHealth;
 
 
+import java.util.Scanner;
+
 import org.bson.Document;
 
 import com.mongodb.client.FindIterable;
@@ -9,14 +11,15 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public class MongoDBCRUD {
+public class ConnectionManager {
 	public static void main(String[] args) {
         // Connexió a MongoDB Atlas
-        String uri = "mongodb+srv://04adrianc:*****@cluster0.mpvz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        String uri = "mongodb+srv://04adrianc:g9SkSHvjMaeNqX6b@cluster0.mpvz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
             MongoDatabase database = mongoClient.getDatabase("SportHealth");
             MongoCollection<Document> collection = database.getCollection("Usuaris");
             System.out.println("Conexión exitosa a MongoDB: " + database.getName());
+            menu();
             FindIterable<Document> documents = collection.find();
             for (Document doc : documents) {
                 System.out.println(doc.toJson());
@@ -25,4 +28,11 @@ public class MongoDBCRUD {
             System.err.println("Error al conectar a MongoDB: " + e.getMessage());
         }
     }
+	public static void menu() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Menu");
+		System.out.println("1. Insereix una document");
+		System.out.println("2. Obtenir tots els documents");
+		System.out.println("3. Obtenir documents per data");
+	}
 }
