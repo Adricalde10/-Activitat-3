67353f877de0c22f69436a50f63290564d23820d
adri <04adrianc@inscastellet.cat>
2025-02-04 19:34:38 +0100

    Canvis en el menu

--- SportHealth/src/main/java/castellet/adrian/SportHealth/ConnectionManager.java
+++ SportHealth/src/main/java/castellet/adrian/SportHealth/ConnectionManager.java
@@ -12,18 +12,20 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class ConnectionManager {
+    static String uri = "mongodb+srv://04adrianc:g9SkSHvjMaeNqX6b@cluster0.mpvz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    static MongoClient mongoClient = MongoClients.create(uri);
+    static MongoDatabase database = mongoClient.getDatabase("SportHealth");
+    static MongoCollection<Document> collection = database.getCollection("Usuaris");
 	public static void main(String[] args) {
         // Connexió a MongoDB Atlas
-        String uri = "mongodb+srv://04adrianc:g9SkSHvjMaeNqX6b@cluster0.mpvz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("SportHealth");
-            MongoCollection<Document> collection = database.getCollection("Usuaris");
+        //String uri = "mongodb+srv://04adrianc:g9SkSHvjMaeNqX6b@cluster0.mpvz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        try {
+            //MongoDatabase database = mongoClient.getDatabase("SportHealth");
+            //MongoCollection<Document> collection = database.getCollection("Usuaris");
             System.out.println("Conexión exitosa a MongoDB: " + database.getName());
             menu();
-            FindIterable<Document> documents = collection.find();
-            for (Document doc : documents) {
-                System.out.println(doc.toJson());
-            }
+            
+            
         }  catch (Exception e) {
             System.err.println("Error al conectar a MongoDB: " + e.getMessage());
         }
@@ -34,5 +36,15 @@ public class ConnectionManager {
 		System.out.println("1. Insereix una document");
 		System.out.println("2. Obtenir tots els documents");
 		System.out.println("3. Obtenir documents per data");
-	}
+		int op = scanner.nextInt();
+		switch(op) {
+		case 1:
+			System.out.println("opcio 1");
+		case 2:
+			FindIterable<Document> documents = collection.find();
+            for (Document doc : documents) {
+                System.out.println(doc.toJson());
+            }
+		}
+		}
 }
